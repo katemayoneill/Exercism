ab83318232711a94670f4510fa1ee18c34174f6a
katemayoneill <oneilk10@tcd.ie>
2023-10-18 13:18:49 +0100

    finishes triangle exercise

--- prolog/triangle/triangle.pl
+++ prolog/triangle/triangle.pl
@@ -1,1 +1,25 @@
-triangle(Side1, Side2, Side3, Type).
+valid_triangle(Side1, Side2, Side3) :-
+    (Side1 > 0, 
+        Side2 > 0, 
+        Side3 > 0),
+    (Side1 + Side2 >= Side3,
+        Side1 + Side3 >= Side2,
+        Side2 + Side3 >= Side1).
+    
+triangle(Side1, Side2, Side3, "equilateral") :-  
+    valid_triangle(Side1, Side2, Side3),
+    Side1 =:= Side2,
+    Side1 =:= Side3.
+
+triangle(Side1, Side2, Side3, "isosceles") :-  
+    valid_triangle(Side1, Side2, Side3),
+    (Side1 =:= Side2;
+        Side1 =:= Side3;
+        Side2 =:= Side3).
+        
+triangle(Side1, Side2, Side3, "scalene") :-
+    valid_triangle(Side1, Side2, Side3),
+    (Side1 \= Side2,
+        Side1 \= Side3,
+        Side2 \= Side3).
+
